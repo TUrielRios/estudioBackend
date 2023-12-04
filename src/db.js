0ea0d82dfd8e7d53e5e6d81c8c81c4e62a63b963
TUrielRios <riostiziano6@gmail.com>
2023-12-04 17:33:46 -0300

    deploy si

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-reqiuire('pg')
+require('pg')
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 } = process.env;
